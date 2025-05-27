6e650d4cd15e9ccef66c9750d58f2a924c81f4ca
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-27 16:11:18 -0500

    got started in java
    got started in java

---
+++ Variables/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // primitive data types
+        boolean val1 = true;
+
+        // single character
+        char c = 'a';
+
+        // Numerics
+        // whole numbers
+        byte b; //-128 to 127
+        short s; // -32,768 to 32,768
+        int i; // -2B to +2B
+        long l = 9_000_000; // really really big
+
+        //floating point (decimals)
+        float pi // 7 decimal places of percision
+        double d;  // 15 decimal places of percision
+
+    }
+}
