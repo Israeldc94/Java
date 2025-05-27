708bbf356a9d017474f154b4e2684cf45748526e
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-27 16:40:35 -0500

    Data types exercise progress

--- exercise-data-types-and-variables/src/Main.java
+++ exercise-data-types-and-variables/src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String playerName = "Christiano Ronaldo";
+        int jerseyNumber = 7;
+        String position = "Forward";
+        boolean isStarter = true;
+        String teamName = "Al-Nassr FC";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String movieTitle = "Inception";
+        int releaseYear = 2010;
+        String rating = "PG-13";
+        boolean isSequel = false;
+        String leadActor = "Leonardo DeCaprio";
+
+        String cityName = "Jarrell";
+        String temperature = "85 degrees Fahrenheit";
+        boolean isRaining = false;
+        String humidity = "56%";
+        String weatherCondition = "Partly Cloudy";
+
+        String flightNumber = "AA256";
+
     }
 }
