0d77c2cb25889a77d2265d76d9ed96025ca07f89
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-27 17:11:07 -0500

    Update Main.java

--- exercise-data-types-and-variables/src/Main.java
+++ exercise-data-types-and-variables/src/Main.java
@@ -13,12 +13,23 @@ public class Main {
         String leadActor = "Leonardo DeCaprio";
 
         String cityName = "Jarrell";
-        String temperature = "85 degrees Fahrenheit";
+        String temperature = "85 ";
         boolean isRaining = false;
         String humidity = "56%";
-        String weatherCondition = "Partly Cloudy";
+        String weatherCondition = "partly cloudy skies";
 
         String flightNumber = "AA256";
+        String departureCity = "New York";
+        String arrivalCity = "Los Angeles";
+        String gateNumber = "22";
+        String terminal = "Terminal 2";
+        boolean isDelayed = false;
 
+        System.out.println("Soccer Player: " + playerName + " wears jersey number " +
+            jerseyNumber + " and plays as a  " + position + " for " + teamName + "."   );
+        System.out.println("The movie " + movieTitle + " was released in " + releaseYear + " and stars "
+        + leadActor + ".");
+        System.out.println("Weather Report: " + cityName + " has a temperature of " + temperature + "°F with " +
+         weatherCondition + ".");
     }
 }
