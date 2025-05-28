6507a064671bded4b9c28448c5688845229695e6
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-28 13:04:49 -0500

    completed enums exercise
    completed exercise

--- exercise-enums/src/Main.java
+++ exercise-enums/src/Main.java
@@ -7,13 +7,37 @@ public class Main {
         LARGE
     }
 
-    enum SeatSection{
+    enum SeatSection {
         GENERAL,
         PREMIUM,
         VIP
     }
+
+    enum TrafficLight {
+        RED,
+        YELLOW,
+        GREEN
+    }
+
+    enum Weekday {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+
+    enum AlertLevel {
+        LOW,
+        MODERATE,
+        HIGH,
+        SEVERE
+    }
+
     public static void main(String[] args) {
-    CoffeeSize size = CoffeeSize.LARGE;
+        CoffeeSize size = CoffeeSize.LARGE;
         System.out.println(size);
 
         SeatSection general = SeatSection.GENERAL;
@@ -24,5 +48,35 @@ public class Main {
         System.out.println(premium + " is assigned value: " + premium.ordinal());
         System.out.println(vip + " is assigned value: " + vip.ordinal());
 
+        TrafficLight[] signals = TrafficLight.values();
+        System.out.println(signals[1]);
+
+        Weekday workDay = Weekday.WEDNESDAY;
+//        Weekday saturday = Weekday.SATURDAY;
+//        Weekday sunday = Weekday.SUNDAY;
+        boolean isWeekend = (workDay == Weekday.SATURDAY || workDay == Weekday.SUNDAY);
+
+        System.out.println("Workday selected:  " + workDay);
+        System.out.println("Is it a weekend? " + isWeekend);
+
+        AlertLevel currentAlert = AlertLevel.HIGH;
+        System.out.println(currentAlert);
+
+        switch (currentAlert) {
+            case LOW:
+                System.out.println("No immediate danger.");
+                break;
+            case MODERATE:
+                System.out.println("Stay alert and aware.");
+                break;
+            case HIGH:
+                System.out.println("Take precautions and stay informed.");
+                break;
+            case SEVERE:
+                System.out.println("Immediate action required!");
+                break;
+
+
         }
     }
+}
