1406536cf67b1ec45eae3edcb47f62c8063bcebd
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-28 12:35:36 -0500

    Update Main.java

--- exercise-enums/src/Main.java
+++ exercise-enums/src/Main.java
@@ -6,8 +6,23 @@ public class Main {
         MEDIUM,
         LARGE
     }
+
+    enum SeatSection{
+        GENERAL,
+        PREMIUM,
+        VIP
+    }
     public static void main(String[] args) {
     CoffeeSize size = CoffeeSize.LARGE;
         System.out.println(size);
+
+        SeatSection general = SeatSection.GENERAL;
+        SeatSection premium = SeatSection.PREMIUM;
+        SeatSection vip = SeatSection.VIP;
+
+        System.out.println(general + " is assigned value: " + general.ordinal());
+        System.out.println(premium + " is assigned value: " + premium.ordinal());
+        System.out.println(vip + " is assigned value: " + vip.ordinal());
+
         }
     }
