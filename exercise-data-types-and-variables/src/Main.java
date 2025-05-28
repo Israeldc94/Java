25d4ff839bdccb52dc97917681321c5e90863738
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-27 19:34:13 -0500

    Update Main.java

--- exercise-data-types-and-variables/src/Main.java
+++ exercise-data-types-and-variables/src/Main.java
@@ -24,6 +24,13 @@ public class Main {
         String gateNumber = "22";
         String terminal = "Terminal 2";
         boolean isDelayed = false;
+        boolean hasHomework = true;
+        boolean isWeekend = false;
+        boolean attendedClass = true;
+        char firstInitial = 'I';
+        char lastInitial = 'C';
+        char favoriteGrade = 'A';
+
 
         System.out.println("Soccer Player: " + playerName + " wears jersey number " +
             jerseyNumber + " and plays as a  " + position + " for " + teamName + "."   );
@@ -31,5 +38,33 @@ public class Main {
         + leadActor + ".");
         System.out.println("Weather Report: " + cityName + " has a temperature of " + temperature + "°F with " +
          weatherCondition + ".");
+
+        System.out.println("Do I have homework? " + hasHomework);
+        System.out.println("Is it the weekend? " + isWeekend);
+        System.out.println("Did I attend class today? " + attendedClass);
+
+        System.out.println("My first initial is " + firstInitial);
+        System.out.println("My last initial is " + lastInitial);
+        System.out.println("My favorite grade is " + favoriteGrade);
+
+        jerseyNumber = 8;
+        isStarter = false;
+        rating = "PG";
+        isSequel = true;
+        temperature = "95";
+        weatherCondition = "Sunny skies";
+        gateNumber = "33";
+        isDelayed = true;
+
+        System.out.println("Soccer Player: " + playerName + " wears jersey number " +
+                jerseyNumber + " and plays as a  " + position + " for " + teamName + "."   );
+        System.out.println("The movie " + movieTitle + " was released in " + releaseYear + " and stars "
+                + leadActor + ".");
+        System.out.println("Weather Report: " + cityName + " has a temperature of " + temperature + "°F with " +
+                weatherCondition + ".");
+
+
+
+
     }
 }
