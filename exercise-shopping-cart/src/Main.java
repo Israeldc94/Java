dcba8c212cc73d5acb79615c7dabf30fdf2bc18d
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-28 09:29:06 -0500

    shopping cart exercise
    completed exercise

---
+++ exercise-shopping-cart/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int productID = 1;
+    int productCategory = 2;
+    float productCost = 2.56f;
+    float productPrice = 4.99f;
+    int productQuantity = 78;
+    float totalProfit = (productPrice * productQuantity);
+    float trueProfit = (totalProfit - (productCost *productQuantity));
+
+        System.out.println("Total product cost: " + "$" + productCost * productQuantity);
+        System.out.println("Profit Margin: " + ((trueProfit / totalProfit) * 100) + "%");
+        System.out.println("Profit: $" + trueProfit);
+
+
+
+        }
+    }
