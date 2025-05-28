9da3d617cbc21a699601397965819c96db0fddd5
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-28 12:29:17 -0500

    coffee size done
    finished coffee size exercise

--- exercise-enums/src/Main.java
+++ exercise-enums/src/Main.java
@@ -1,8 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    
+    enum CoffeeSize {
+        SMALL,
+        MEDIUM,
+        LARGE
+    }
     public static void main(String[] args) {
-
+    CoffeeSize size = CoffeeSize.LARGE;
+        System.out.println(size);
         }
     }
