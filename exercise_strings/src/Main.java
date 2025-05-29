e4eed54484765dc29673c9dfdefb0a9b488f4709
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-29 18:43:05 -0500

    completed part three of exercise strings
    Completed part 3

--- exercise_strings/src/Main.java
+++ exercise_strings/src/Main.java
@@ -6,7 +6,23 @@ public class Main {
     String lastName = "Potter";
     String fullName = firstName + " " + lastName;
 
-        System.out.println(fullName);
+        System.out.println("Full name: " + fullName);
+        System.out.println("Length: " + fullName.length());
+        System.out.println("First character: " + fullName.charAt(0));
+        System.out.println("Index of 'r': " + fullName.indexOf('r'));
+
+        String sentence = "Learning Java is fun! ";
+
+        System.out.println("First word: " + sentence.substring(0, 8));
+        System.out.println("Second word: " + sentence.substring(9, 13));
+        System.out.println("Last word: " + sentence.substring(17));
+
+        String csvData = "apple,banana,grape,orange";
+        String[] data = csvData.split(",");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("Fruit" + (i+1) + ": " + data[i]);
+
+        }
 
     }
 }
