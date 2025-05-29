32552064af77cb87e726784a636d550db7fff91d
Israeldc94 <custodio.d.israel@gmail.com>
2025-05-29 16:21:10 -0500

    Started exercise strings
    Started exercise realized I skipped the code along saving and returning at a later point

---
+++ exercise_strings/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    String firstName = "Harry";
+    String lastName = "Potter";
+    String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+    }
+}
