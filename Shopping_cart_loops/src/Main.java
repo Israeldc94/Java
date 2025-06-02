0107f627b189b712d7889437e5ec8a7e2328f4a8
Israeldc94 <custodio.d.israel@gmail.com>
2025-06-01 21:07:56 -0500

    shopping cart loops complete
    completed code along exercise

---
+++ Shopping_cart_loops/src/Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the shopping cart app!");
+        java.util.Scanner console = new
+                java.util.Scanner(System.in);
+        double productPrice = 4.99;
+        int productQuantity = 78;
+        double totalCost = productPrice * productQuantity;
+
+        double taxRate = .07;
+        double hundredDollarDiscount = .05;
+        double fiveHundredDollarDiscount = .10;
+
+        double standardShipping = 2.00;
+        double overnightShipping = 10.00;
+        double twoDayShipping = 5.00;
+        boolean isConfirmed = false;
+
+
+        System.out.printf("Product Price: $%.2f", productPrice);
+        System.out.println("\nProduct Quantity: " + productQuantity);
+        System.out.printf("\nTotal Cost: $%.2f", totalCost);
+        double shippingCost = 0;
+        String shipping = null;
+        String promoCode = null;
+        while (!isConfirmed) {
+// Prompt for tax exempt
+            System.out.println("\nAre you tax-exempt? (y/n)");
+            String taxExempt = console.nextLine();
+// Prompt for shipping
+            System.out.println("Shipping? (standard / overnight / two-day)");
+            shipping = console.nextLine();
+// Prompt for promo code
+            System.out.println("Promo code for free shipping?");
+            promoCode = console.nextLine();
+// Print details
+            System.out.println("\nDetails:");
+            // Apply tax, if necessary
+            if (taxExempt.equals("n")) {
+                totalCost = totalCost + (totalCost * taxRate);
+            }
+            System.out.printf("Total w/ tax: $%.2f", totalCost);
+            System.out.println("\nTax-exempt: " + taxExempt);
+            // Apply discount
+            if (totalCost >= 500) {
+                totalCost = totalCost - (totalCost *
+                        fiveHundredDollarDiscount);
+            } else if (totalCost >= 100) {
+                totalCost = totalCost - (totalCost * hundredDollarDiscount);
+            }
+            shippingCost = 0.00;
+            switch (shipping) {
+                case "standard":
+                    shippingCost = standardShipping;
+                    if (promoCode.equals("FREE")) {
+                        shippingCost = 0;
+                    }
+                    break;
+                case "overnight":
+                    shippingCost = overnightShipping;
+                    break;
+                case "twoday":
+                    shippingCost = twoDayShipping;
+                    break;
+                default:
+// bad shipping type
+                    System.out.println("Invalid shipping type");
+            }
+            System.out.println("Confirm Order y/n ");
+            isConfirmed = "y".equals(console.nextLine());
+        }
+        totalCost += shippingCost;
+        System.out.printf("Shipping Cost: $%.2f", shippingCost);
+        System.out.printf("\nFinal Total: $%.2f", totalCost);
+        System.out.printf("\nTotal after discount: $%.2f", totalCost);
+        System.out.println("\nShipping: " + shipping);
+        System.out.println("Promo code: " + promoCode);
+        System.out.println("Bye");
+    }
+}
+
+
+
