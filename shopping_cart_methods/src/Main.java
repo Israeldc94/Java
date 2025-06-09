02f684aa36b6ddee00aed39863b3e4ce913e58f5
Israeldc94 <custodio.d.israel@gmail.com>
2025-06-08 20:20:24 -0500

    completed shopping cart arrays
    completed code along

--- shopping_cart_methods/src/Main.java
+++ shopping_cart_methods/src/Main.java
@@ -1,6 +1,112 @@
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Create arrays to contain addresses and sizes
+        String[] addresses = { "123 Main St", "456 Main St", "789 Main St" };
+                String[] sizes = { "small", "medium", "large" };
+        int addressIndex = 0;
+        int sizeIndex = 0;
+
+        System.out.println("Welcome to the shopping cart app!");
+        java.util.Scanner console = new
+                java.util.Scanner(System.in);
+        double productPrice = 4.99;
+        int productQuantity = 78;
+        double totalCost = productPrice * productQuantity;
+
+        double taxRate = .07;
+        double hundredDollarDiscount = .05;
+        double fiveHundredDollarDiscount = .10;
+
+        double standardShipping = 2.00;
+        double overnightShipping = 10.00;
+        double twoDayShipping = 5.00;
+        boolean isConfirmed = false;
+
+
+        System.out.printf("Product Price: $%.2f", productPrice);
+        System.out.println("\nProduct Quantity: " + productQuantity);
+        System.out.printf("\nTotal Cost: $%.2f", totalCost);
+        double shippingCost = 0;
+        String shipping = null;
+        String promoCode = null;
+        while (!isConfirmed) {
+// Prompt for tax exempt
+            System.out.println("\nAre you tax-exempt? (y/n)");
+            String taxExempt = console.nextLine();
+// Prompt for shipping
+            System.out.println("Shipping? (standard / overnight / two-day)");
+            shipping = console.nextLine();
+//Prompt for address
+            for (int i = 0; i < addresses.length; i++) {
+                System.out.println((i + 1) + ". " + addresses[i]);
+            }
+
+            System.out.println("Enter your shipping address: ");
+            String address = console.nextLine();
+            addressIndex = Integer.parseInt(address);
+
+//prompt for size
+            // Prompt for Size
+            for (int i = 0; i < sizes.length; i++) {
+                System.out.println((i + 1) + ". " + sizes[i]);
+            }
+            System.out.println("Size?");
+            String size = console.nextLine();
+            sizeIndex = Integer.parseInt(size);
+
+// Prompt for promo code
+            System.out.println("Promo code for free shipping?");
+            promoCode = console.nextLine();
+// Print details
+            // Print details
+            System.out.println("\nDetails:");
+            System.out.println("Address: " + addresses[addressIndex - 1]);
+            System.out.println("Size: " + sizes[sizeIndex - 1]);
+
+            // Apply tax, if necessary
+            if (taxExempt.equals("n")) {
+                totalCost = totalCost + (totalCost * taxRate);
+            }
+            System.out.printf("Total w/ tax: $%.2f", totalCost);
+            System.out.println("\nTax-exempt: " + taxExempt);
+            // Apply discount
+            if (totalCost >= 500) {
+                totalCost = totalCost - (totalCost *
+                        fiveHundredDollarDiscount);
+            } else if (totalCost >= 100) {
+                totalCost = totalCost - (totalCost * hundredDollarDiscount);
+            }
+            shippingCost = 0.00;
+            switch (shipping) {
+                case "standard":
+                    shippingCost = standardShipping;
+                    if (promoCode.equals("FREE")) {
+                        shippingCost = 0;
+                    }
+                    break;
+                case "overnight":
+                    shippingCost = overnightShipping;
+                    break;
+                case "twoday":
+                    shippingCost = twoDayShipping;
+                    break;
+                default:
+// bad shipping type
+                    System.out.println("Invalid shipping type");
+            }
+            System.out.println("Confirm Order y/n ");
+            isConfirmed = "y".equals(console.nextLine());
+        }
+        totalCost += shippingCost;
+        System.out.printf("Shipping Cost: $%.2f", shippingCost);
+        System.out.printf("\nFinal Total: $%.2f", totalCost);
+        System.out.printf("\nTotal after discount: $%.2f", totalCost);
+        System.out.println("\nShipping: " + shipping);
+        System.out.println("Promo code: " + promoCode);
+        System.out.println("Bye");
     }
 }
