24c436f7fb5e538bc715c79b361c8a703ac41f30
Israeldc94 <custodio.d.israel@gmail.com>
2025-06-08 19:56:21 -0500

    completed exercise arrays
    completed exercise

--- exercise Arrays/src/Main.java
+++ exercise Arrays/src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        Random rand = new Random();
 
         String[] cities = {"San antonio", "San marcos", "Austin", "New york", "San diego"};
 
@@ -25,21 +27,21 @@ public class Main {
         System.out.println("Enter a city");
         String input = console.nextLine();
         for (int i = 0; i < cities.length; i++) {
-            if (input.equals(cities[i])){
+            if (input.equals(cities[i])) {
                 found = true;
                 break;
             }
         }
-        if (found);
+        if (found) ;
         System.out.println("City found!");
 
-        int [] scores = {90, 85, 70, 95, 100};
+        int[] scores = {90, 85, 70, 95, 100};
 
         int sum = 0;
         for (int i = 0; i < scores.length; i++) {
             sum += scores[i];
         }
-            System.out.println(sum);
+        System.out.println(sum);
 
         int highest = 0;
         for (int i = 0; i < scores.length; i++) {
@@ -57,9 +59,51 @@ public class Main {
         }
         System.out.println(lowest);
 
-        int average = sum/scores.length;
+        int average = sum / scores.length;
 
         System.out.println(average);
+
+        int[] values = new int[10];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = rand.nextInt(6) + 1;
+        }
+        int count = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == 3) {
+                count++;
+            }
+        }
+        System.out.println("3 appears " + count + " times.");
+
+        int[] nums = {1, 2, 3, 4, 5};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        int first = nums[0];
+        for (int i = 0; i < nums.length -1; i++) {
+            nums[i] = nums[i+1];
+        }
+        nums[nums.length - 1] = first;
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        String [] names = {"Jack", "Jill", "Jack", "Sam", "Smith","Luther"};
+        boolean duplicate = false;
+        for(int i =0; i < names.length; i++){
+            for(int j = i +1; j < names.length; j++){
+                if (names[i].equals(names[j])){
+                    duplicate = true;
+                    break;
+                }
+            }
+            if(duplicate){
+                break;
+            }
+        }
+            if (duplicate){
+                System.out.println("Duplicates found");
+            }else{
+                System.out.println("Duplicates not found");
+            }
     }
 }
-
