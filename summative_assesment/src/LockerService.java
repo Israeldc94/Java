51bdd6b1d9f5b6bc57150cd232fba7dfc5e1cf04
Israeldc94 <custodio.d.israel@gmail.com>
2025-06-11 11:19:31 -0500

    revising summative assesment

--- summative_assesment/src/LockerService.java
+++ summative_assesment/src/LockerService.java
@@ -54,58 +54,78 @@ public class LockerService {
         return new Result(false, "\nLocker not found.");
     }
 
-            /*gets a locker number from the user and checks the list for a matching number the function then requests a pin if they find a match for that locker.
-            After confirming with the user the locker is then released.
-             */
-            public Result releaseLocker () {
-                int lockerNumber = io.getInt("\nEnter your locker number: ", 1, lockers.length);
-                for (int i = 0; i < lockers.length; i++) {
-                    Locker locker = lockers[i];
-                    if (locker.getLockerNumber() == lockerNumber)
-                        if (locker.isRented()) {
-                            String pin = io.getValidPin("\nEnter your pin number: ");
-                            if (locker.getPinNumber().equals(pin)) {
-                                String choice = io.getNonEmptyString("\nAre you sure? (y/n)");
-                                if (choice.equals("y")) {
-                                    locker.release();
-                                    return new Result(true, lockerNumber + " is released! We hope you enjoyed the show.");
-                                } else if (choice.equals("n")) {
-                                    return new Result(false, lockerNumber + " is still being held. Enjoy the show.");
-
-                                } else {
-                                    return new Result(false, "\nInvalid input please select (y)es or (n)o");
-                                }
-                            } else {
-                                return new Result(false, "\nThis pin number is invalid");
-                            }
+    /*gets a locker number from the user and checks the list for a matching number the function then requests a pin if they find a match for that locker.
+    After confirming with the user the locker is then released.
+     */
+    public Result releaseLocker() {
+        int lockerNumber = io.getInt("\nEnter your locker number: ", 1, lockers.length);
+        for (int i = 0; i < lockers.length; i++) {
+            Locker locker = lockers[i];
+            if (locker.getLockerNumber() == lockerNumber)
+                if (locker.isRented()) {
+                    String pin = io.getValidPin("\nEnter your pin number: ");
+                    if (locker.getPinNumber().equals(pin)) {
+                        String choice = io.getNonEmptyString("\nAre you sure? (y/n)");
+                        if (choice.equals("y")) {
+                            locker.release();
+                            return new Result(true, lockerNumber + " is released! We hope you enjoyed the show.");
+                        } else if (choice.equals("n")) {
+                            return new Result(false, lockerNumber + " is still being held. Enjoy the show.");
 
                         } else {
-                            return new Result(false, "This locker is not rented");
+                            return new Result(false, "\nInvalid input please select (y)es or (n)o");
                         }
-                }
-                return new Result(false, "\nLocker not found");
-            }
-
-            //sets up a random 2-digit number and concatenates it with "00" to create a pin
-            public String setPinNumber () {
-                Random rand = new Random();
-                int randomNumber = rand.nextInt(90) + 10;
-                String pin = "00" + randomNumber;
-                return pin;
-            }
-
-            //checks if all lockers are rented we will use this to print our alternative menu option
-            public boolean lockersOccupied () {
-                for (int i = 0; i < lockers.length; i++) {
-                    if (!lockers[i].isRented()) {
-                        return false;
+                    } else {
+                        return new Result(false, "\nThis pin number is invalid");
                     }
+
+                } else {
+                    return new Result(false, "This locker is not rented");
                 }
-                return true;
+        }
+        return new Result(false, "\nLocker not found");
+    }
 
+    //sets up a random 2-digit number and concatenates it with "00" to create a pin
+    public String setPinNumber() {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(90) + 10;
+        String pin = "00" + randomNumber;
+        return pin;
+    }
 
+    //checks if all lockers are rented we will use this to print our alternative menu option
+    public boolean lockersOccupied() {
+        for (int i = 0; i < lockers.length; i++) {
+            if (!lockers[i].isRented()) {
+                return false;
             }
         }
+        return true;
+
+
+    }
+
+    public Locker lockerValidation() {
+        int lockerNumber = io.getInt("\nEnter your locker number: ", 1, lockers.length);
+        for (int i = 0; i < lockers.length; i++) {
+            Locker locker = lockers[i];
+            if (locker.getLockerNumber() == lockerNumber) {
+                if (locker.isRented()) {
+                    String pin = io.getValidPin("\nEnter your pin number: ");
+                    if (locker.getPinNumber().equals(pin)) {
+                        return locker;
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}
+
 
 
 
