8fa157418498957f58c0b747696a5db77c940bce
Israeldc94 <custodio.d.israel@gmail.com>
2025-07-10 15:59:52 -0500

    started capstone.
    wired up spring

---
+++ capstone-inventory/src/main/java/com/example/capstone_inventory/CapstoneInventoryApplication.java
@@ -0,0 +1,23 @@
+package com.example.capstone_inventory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class CapstoneInventoryApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CapstoneInventoryApplication.class, args);
+
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+}
---
+++ capstone-inventory/src/test/java/com/example/capstone_inventory/CapstoneInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
